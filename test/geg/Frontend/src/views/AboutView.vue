63c215baded33bb019fdd2ddc2cd6a7285aa6295
TroyDemafeliz <200620227@my.xu.edu.ph>
2024-04-29 20:51:48 +0800

    added dummy images and updated about us page

--- test/geg/Frontend/src/views/AboutView.vue
+++ test/geg/Frontend/src/views/AboutView.vue
@@ -1,6 +1,69 @@
 <template>
-  <div class = "about">
-    <h1>This is an about page</h1>
+  <div>
+    <div class="flex flex-col items-center justify-center text-center px-4 pt-20 container mx-auto">
+        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-red-geg md:text-5xl lg:text-6xl dark:text-white">About Us</h1>
+        <p class="mb-10 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400 max-w-prose">
+          Learn about us and meet our team.
+        </p>
+      </div>
+
+      <div class="container mx-auto px-4">
+          <div class="pt-20 pb-7">
+          </div>
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-4 pb-20">
+              <img class="w-full h-auto rounded-lg" src="../assets/se-dummy-images/project-5 (school).jpeg" alt="image description">
+                  <div>
+                    <h1 class="text-4xl md:text-5xl lg:text-6xl font-extrabold leading-none tracking-tight text-red-geg dark:text-white mb-4 pl-5">History</h1>
+                    <p class="text-gray-500 dark:text-gray-400 text-justify pl-5">
+                        GEG Construction adopts a meticulous and strategic approach to every project, 
+                        ensuring seamless execution from start to finish. Initially, they conduct a 
+                        thorough site assessment, which helps in identifying potential challenges and opportunities, 
+                        followed by detailed client consultations to align project goals with realistic expectations and feasible timelines. 
+                        Their commitment to sustainability is evident in the selection of materials and construction practices, emphasizing 
+                        energy efficiency and minimal environmental impact. 
+                    </p>
+                  </div>
+          </div>
+      </div>
+
+      <div class="container mx-auto px-4">
+        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-red-geg md:text-5xl lg:text-6xl dark:text-white pb-10 text-center">Meet our Team</h1>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 pb-20 flex-grow flex-col">
+                  <div>
+                    <img class="w-full h-auto rounded-lg" src="../assets/se-dummy-images/dummy-pic-1-abt-us.jpeg" alt="image description">
+                    <h1 class="mt-8 text-4xl font-extrabold leading-none tracking-tight text-black md:text-3xl lg:text-4xl dark:text-white text-center">John Doe</h1>
+                    <h1 class="mt-1 text-4xl font-extralight leading-none tracking-tight text-gray-500 md:text-2xl lg:text-3xl dark:text-white text-center">Construction Specialist</h1>
+                      <p class="mt-10 text-lg font-extralight text-black lg:text-xl dark:text-gray-400 max-w-prose text-center justify-center mx-5">
+                        John is passionate about providing top-notch construction services and ensuring customer satisfaction.
+                      </p>
+                  </div>
+                  <div>
+                    <img class="w-full h-auto rounded-lg" src="../assets/se-dummy-images/dummy-pic-2-abt-us.jpeg" alt="image description">
+                    <h1 class="mt-8 text-4xl font-extrabold leading-none tracking-tight text-black md:text-3xl lg:text-4xl dark:text-white text-center">Michael Johnson</h1>
+                    <h1 class="mt-1 text-4xl font-extralight leading-none tracking-tight text-gray-500 md:text-2xl lg:text-3xl dark:text-white text-center"> Customer Service</h1>
+                      <p class="mt-10 text-lg font-extralight text-black lg:text-xl dark:text-gray-400 max-w-prose text-center justify-center mx-5">
+                        Michael is dedicated to providing outstanding customer service and ensuring a positive experience for every client.
+                      </p>
+                  </div>
+                  <div>
+                    <img class="w-full h-auto rounded-lg" src="../assets/se-dummy-images/dummy-pic-3-abt-us.jpeg" alt="image description">
+                    <h1 class="mt-8 text-4xl font-extrabold leading-none tracking-tight text-black md:text-3xl lg:text-4xl dark:text-white text-center">Jane Smith Doe</h1>
+                    <h1 class="mt-1 text-4xl font-extralight leading-none tracking-tight text-gray-500 md:text-2xl lg:text-3xl dark:text-white text-center">Detailing Expert</h1>
+                      <p class="mt-10 text-lg font-extralight text-black lg:text-xl dark:text-gray-400 max-w-prose text-center justify-center mx-5">
+                        Jane has extensive experience in construction detailing and takes pride in delivering exceptional results.
+                      </p>
+                  </div>
+                  <div>
+                    <img class="w-full h-auto rounded-lg" src="../assets/se-dummy-images/dummy-pic-4-abt-us.jpeg" alt="image description">
+                    <h1 class="mt-8 text-4xl font-extrabold leading-none tracking-tight text-black md:text-3xl lg:text-4xl dark:text-white text-center">Saroh Wilson</h1>
+                    <h1 class="mt-1 text-4xl font-extralight leading-none tracking-tight text-gray-500 md:text-2xl lg:text-3xl dark:text-white text-center">Quality Control Manager</h1>
+                      <p class="mt-10 text-lg font-extralight text-black lg:text-xl dark:text-gray-400 max-w-prose text-center justify-center mx-5">
+                        Sarah ensures that every carwash meets our high standards of quality and customer satisfaction.
+                      </p>
+                  </div>
+            </div>
+      </div>
+
   </div>
 </template>
 
