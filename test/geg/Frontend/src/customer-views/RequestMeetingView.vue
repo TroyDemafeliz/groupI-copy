8f70d379c91234ed7ff4e10e7aed0a34744ff190
DavidRomanillos <20210022695@my.xu.edu.ph>
2024-05-01 15:48:45 +0800

    fixed margins of form

--- test/geg/Frontend/src/customer-views/RequestMeetingView.vue
+++ test/geg/Frontend/src/customer-views/RequestMeetingView.vue
@@ -5,7 +5,7 @@
               <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl whitespace-nowrap"><span class="text-transparent bg-clip-text bg-gradient-to-r to-red-400 from-red-600">Request Meeting</span></h1>
               <p class="text-lg font-normal text-black lg:text-xl dark:text-black mb-10 mt-5">Welcome to our meeting request page. We look forward to discussing your construction needs</p>
         </div>
-        <div class="border-2 border-gray-300 p-10">
+        <div class="border-2 border-gray-300 p-20 rounded-md">
               <div class="grid md:grid-cols-2 md:gap-6">
   
                     <div class="relative z-0 w-full mb-5 group">
@@ -36,12 +36,12 @@
               </div>
   
               <form class="max-w-lg mx-auto">
-              <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Upload file</label>
+              <label class="block mt-5 mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Upload file</label>
               <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file">
-              </form>
-        
-        </div>
-        <button type="submit" class="text-white mt-5 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 justify-end">Submit</button>
+              <button type="submit" class="block mx-auto text-white mt-10 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-20 py-3 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 ">Submit</button>
+            </form>
+            
+          </div>
         </form>
   </form>
   </template>
