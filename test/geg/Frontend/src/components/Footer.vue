602d460fc34ebee339b4b7f5c4e068c72b1e121d
TroyDemafeliz <200620227@my.xu.edu.ph>
2024-05-26 12:01:40 +0800

    dockerfile modified and footer fixed vertically

--- test/geg/Frontend/src/components/Footer.vue
+++ test/geg/Frontend/src/components/Footer.vue
@@ -1,44 +1,44 @@
 <template>
-    <div class="mx-auto bg-blue-geg text-white h-[500px]">
-        <div class="text-center py-8 indent-10">
-            <div class="py-4 text-left text-white mb-10">
-                <h2 class="text-4xl font-semibold">Contact Us</h2>
-                <p class="text-xl text-white">We're here to answer any questions you may have.</p>
-            </div>
-        </div>
-        <div class="flex flex-wrap -mx-4">
-            <div class="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
-                <img class="w-10 h-10 mx-auto mb-4" alt="" src="./icons/envelope.png" />
-                <div class="text-center">
-                    <h3 class="text-xl font-semibold">Email</h3>
-                    <p class="text-sm  mb-4">Send us an email and we'll get back to you.</p>
-                    <a class="text-blue-500" href="mailto:gegbcs@gmail.com" target="_blank">gegbcs@gmail.com</a>
+        <footer class="mx-auto bg-blue-geg text-white">
+            <div class="text-center py-8 indent-10">
+                <div class="py-4 text-left text-white mb-10">
+                    <h2 class="text-4xl font-semibold">Contact Us</h2>
+                    <p class="text-xl text-white">We're here to answer any questions you may have.</p>
                 </div>
             </div>
-            <div class="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
-                <img class="w-10 h-10 mx-auto mb-4" alt="" src="./icons/message-detail.png" />
-                <div class="text-center">
-                    <h3 class="text-xl font-semibold">Facebook</h3>
-                    <p class="text-sm  mb-4">Chat with our team in real-time for immediate assistance.</p>
-                    <a class="text-blue-500" href="https://www.facebook.com/profile.php?id=100063788417726" target="_blank">Facebook Link</a>
+            <div class="flex flex-wrap -mx-4">
+                <div class="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
+                    <img class="w-10 h-10 mx-auto mb-4" alt="" src="./icons/envelope.png" />
+                    <div class="text-center">
+                        <h3 class="text-xl font-semibold">Email</h3>
+                        <p class="text-sm  mb-4">Send us an email and we'll get back to you.</p>
+                        <a class="text-blue-500" href="mailto:gegbcs@gmail.com" target="_blank">gegbcs@gmail.com</a>
+                    </div>
+                </div>
+                <div class="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
+                    <img class="w-10 h-10 mx-auto mb-4" alt="" src="./icons/message-detail.png" />
+                    <div class="text-center">
+                        <h3 class="text-xl font-semibold">Facebook</h3>
+                        <p class="text-sm  mb-4">Chat with our team in real-time for immediate assistance.</p>
+                        <a class="text-blue-500" href="https://www.facebook.com/profile.php?id=100063788417726" target="_blank">Facebook Link</a>
+                    </div>
+                </div>
+                <div class="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
+                    <img class="w-10 h-10 mx-auto mb-4" alt="" src="./icons/phone.png" />
+                    <div class="text-center">
+                        <h3 class="text-xl font-semibold">Phone</h3>
+                        <p class="text-sm  mb-4">Give us a call and speak with our friendly team.</p>
+                        <a class="text-blue-500" href="tel:09663206811" target="_blank">0966 320 6811</a>
+                    </div>
+                </div>
+                <div class="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
+                    <img class="w-10 h-10 mx-auto mb-4" alt="" src="./icons/map.png" />
+                    <div class="text-center">
+                        <h3 class="text-xl font-semibold">Office</h3>
+                        <p class="text-sm  mb-4">Visit our office for a face-to-face consultation.</p>
+                        <a class="text-blue-500" href="https://www.google.com/maps" target="_blank">Zone 2 Canitoan 9000 Cagayan de Oro, Philippines</a>
+                    </div>
                 </div>
             </div>
-            <div class="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
-                <img class="w-10 h-10 mx-auto mb-4" alt="" src="./icons/phone.png" />
-                <div class="text-center">
-                    <h3 class="text-xl font-semibold">Phone</h3>
-                    <p class="text-sm  mb-4">Give us a call and speak with our friendly team.</p>
-                    <a class="text-blue-500" href="tel:09663206811" target="_blank">0966 320 6811</a>
-                </div>
-            </div>
-            <div class="w-full md:w-1/2 lg:w-1/4 px-4 mb-8">
-                <img class="w-10 h-10 mx-auto mb-4" alt="" src="./icons/map.png" />
-                <div class="text-center">
-                    <h3 class="text-xl font-semibold">Office</h3>
-                    <p class="text-sm  mb-4">Visit our office for a face-to-face consultation.</p>
-                    <a class="text-blue-500" href="https://www.google.com/maps" target="_blank">Zone 2 Canitoan 9000 Cagayan de Oro, Philippines</a>
-                </div>
-            </div>
-        </div>
-    </div>
+        </footer>
 </template>
