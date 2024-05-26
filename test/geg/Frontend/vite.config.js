abc3002bb1bd2a2f1607b953ecc7cd348e4dffc1
TroyDemafeliz <200620227@my.xu.edu.ph>
2024-05-26 17:26:47 +0800

    minor fixes for deployment

--- test/geg/Frontend/vite.config.js
+++ test/geg/Frontend/vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
     vueJsx(),
     VueDevTools(),
   ],
+  build: {
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
