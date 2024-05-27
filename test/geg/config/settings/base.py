ca4a660ca12c00f398000f7f0e239337d71e1e17
TroyDemafeliz <200620227@my.xu.edu.ph>
2024-05-27 08:01:27 +0800

    added group-i website allowed origins

--- test/geg/config/settings/base.py
+++ test/geg/config/settings/base.py
@@ -123,6 +123,9 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://groupi-copy.onrender.com",
+]
 CORS_ALLOWS_CREDENTIALS = True
 
 THIRD_PARTY_APPS = [
