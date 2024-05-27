78b406e36598346d8359d3917d73a89decc87e3b
TroyDemafeliz <200620227@my.xu.edu.ph>
2024-05-27 08:23:50 +0800

    allowed all origins

--- test/geg/config/settings/base.py
+++ test/geg/config/settings/base.py
@@ -123,9 +123,6 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "https://groupi-copy.onrender.com",
-]
 CORS_ALLOWS_CREDENTIALS = True
 
 THIRD_PARTY_APPS = [
